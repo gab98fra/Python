145771a5c3470da62220c288168ca487ce38a17f
Gabriel F <gabriel_frac@outlook.com>
2020-12-03 22:47:08 -0600

    Api

---
+++ api/index.py
@@ -0,0 +1,19 @@
+# ----------------------------------------------------------------------------
+# Nombre:       get_html.py
+# Autor:        Gabriel F
+# GitHub:       https://github.com/gab98fra/
+# Creado:       04 de diciembre 2020
+# Modificado:   04 de diciembre 2020
+# Copyright:    (c) 2020 by Gabriel F, 2020
+# ----------------------------------------------------------------------------
+
+"""
+    API
+
+    Python 3.8.2
+
+"""
+
+
+
+
